c127cfa0cca144642069013cbf3cb037b63520fe
SamuelFST <guti.samuel2002@gmail.com>
2022-08-08 19:27:17 -0300

    chore: inputs and stylesheets on LoginScreen and RegisterScreen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function LoginScreen() {
+import { Context as AuthContext } from '../context/AuthContext';
+import Spacer from "../components/Spacer";
+import AuthForm from "../components/AuthForm";
+
+interface Props {
+  navigation: NativeStackNavigationProp<any, any>;
+}
+
+export default function LoginScreen({ navigation }: Props) {
+  const { login } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        submitButtonText="Login"
+        onSubmit={login}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+        <Spacer>
+          <Text style={styles.link}>
+            Não tem uma conta? Faça o cadastro
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "stretch",
+    marginBottom: 150,
+    marginTop: 100,
+  },
+  link: {
+    color: "blue",
+    textAlign: "center",
+  },
 });
--- screens/ResgiterScreen.tsx
+++ screens/ResgiterScreen.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function RegisterScreen() {
+import Spacer from "../components/Spacer";
+import AuthForm from "../components/AuthForm";
+
+interface Props {
+  navigation: NativeStackNavigationProp<any, any>;
+}
+
+export default function RegisterScreen({ navigation }: Props) {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>ResgiterScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        submitButtonText="Cadastrar-se"
+        onSubmit={({ user, password }: { user: string, password: string }) => {}}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Spacer>
+          <Text style={styles.link}>
+            Já tem uma conta? Faça o login
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "stretch",
+    marginBottom: 150,
+    marginTop: 100,
+  },
+  link: {
+    color: "blue",
+    textAlign: "center",
+  },
 });
