7d6ad7ffedf8f4b6607cf48b864fafeb3efab0a1
SamuelFST <guti.samuel2002@gmail.com>
2022-08-08 22:25:52 -0300

    feat: secureStore, register and tryLocalLogin

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement, useReducer } from "react";
 import jwtDecode from "jwt-decode";
+import * as SecureStore from "expo-secure-store";
 
 import axios from "../api/axios";
+import { navigate } from "../RootNavigation";
 
 interface TokenUser {
   profile: string;
@@ -17,10 +19,26 @@ interface IAuthContext {
   token: string | null;
   user: string | null;
   profile: string | null;
-  login?: (dispatch: any) => void;
+  errorMessage?: string | null;
+  isLoading: boolean;
+  login?: () => void;
+  clearErrorMessage?: () => void;
+  register?: () => void;
+  tryLocalLogin?: () => void;
 }
 
-const defaultValue = { token: null, user: null, profile: null };
+interface Login {
+  user: string;
+  password: string;
+}
+
+const defaultValue = {
+  token: null,
+  user: null,
+  profile: null,
+  errorMessage: null,
+  isLoading: true,
+};
 
 const Context = React.createContext<IAuthContext>(defaultValue);
 
@@ -28,7 +46,16 @@ const Provider = ({ children }: { children: ReactElement }) => {
   const reducer = (state: any, action: Action) => {
     switch(action.type) {
       case "login": {
-        return { ...state, ...action.payload };
+        return { ...state, ...action.payload, errorMessage: null, isLoading: false };
+      }
+      case "user_created": {
+        return { ...state, errorMessage: null };
+      }
+      case "add_error": {
+        return { ...state, errorMessage: action.payload };
+      }
+      case "clear_error_message": {
+        return { ...state, errorMessage: null };
       }
       default: {
         return state;
@@ -39,7 +66,7 @@ const Provider = ({ children }: { children: ReactElement }) => {
   const [state, dispatch] = useReducer(reducer, defaultValue);
 
   const login = (dispatch: any) => {
-    return async ({ user, password }: { user: string, password: string }) => {
+    return async ({ user, password }: Login) => {
       try {
         const response = await axios.post('/security/login', {
           user,
@@ -48,15 +75,63 @@ const Provider = ({ children }: { children: ReactElement }) => {
         const { accessToken } = response.data;
         const { profile, user: userName } = jwtDecode(accessToken) as TokenUser;
 
-        dispatch({ type: "login", payload: { token: accessToken, profile, user: userName } })
+        await SecureStore.setItemAsync("token", accessToken);
+        await SecureStore.setItemAsync("user", userName);
+        await SecureStore.setItemAsync("profile", profile);
+
+        dispatch({ type: "login", payload: { token: accessToken, profile, user: userName } });
       } catch (err) {
-        console.log(err);
+        dispatch({
+          type: "add_error",
+          payload: "Ocorreu um erro ao fazer login, verifique o usuário e senha e tente novamente",
+        });
       }
     }
   };
 
+  const register  =
+    (dispatch: any) =>
+    async ({ user, password }: Login) => {
+      try {
+        await axios.post('/security/register', {
+          user,
+          password,
+        });
+
+        dispatch({ type: "user_created" });
+        navigate("Login");
+      } catch (err) {
+        dispatch({ type: "add_error", payload: "Ocorreu um erro ao fazer o cadastro" });
+      }
+    }
+
+  const clearErrorMessage = (dispatch: any) => () => {
+    dispatch({ type: "clear_error_message" });
+  }
+
+  const tryLocalLogin = (dispatch: any) => async () => {
+    let token, user, profile;
+    try {
+      token = await SecureStore.getItemAsync("token");
+      user = await SecureStore.getItemAsync("user");
+      profile = await SecureStore.getItemAsync("profile");
+
+      dispatch({ type: "login", payload: { token, user, profile } });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <Context.Provider value={{ ...state, login: login(dispatch) }}>
+    <Context.Provider
+      value={{
+        ...state,
+        login: login(dispatch),
+        clearErrorMessage: clearErrorMessage(dispatch),
+        register: register(dispatch),
+        tryLocalLogin: tryLocalLogin(dispatch),
+      }}
+    >
       {children}
     </Context.Provider>
   );
