2cf6a5ffa572d44f2c980514faf775df35f24324
SamuelFST <guti.samuel2002@gmail.com>
2022-08-10 20:02:46 -0300

    chore: custom icons on Tab Navigator

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { useContext, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/ResgiterScreen';
@@ -39,10 +39,25 @@ const App = () => {
             <Stack.Screen name='Register' component={RegisterScreen} />
           </Stack.Navigator>
         ) : (
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                switch(route.name) {
+                  case "Home": {
+                    return (<MaterialIcons name='home' size={size} color={color} />);
+                  }
+                  case "Profiles": {
+                    return (<MaterialIcons name='groups' size={size} color={color} />);
+                  }
+                  case "Profile": {
+                    return (<MaterialIcons name='account-circle' size={size} color={color} />);
+                  }
+                }
+              },
+              headerShown: false,
+            })}
+          >
             <Tab.Screen name='Home' component={HomeScreen} />
-            <Tab.Screen name='Post' component={PostScreen} />
-            <Tab.Screen name='CreatePost' component={NewPostScreen} />
             <Tab.Screen name='Profiles' component={ProfilesScreen} />
             <Tab.Screen name='Profile' component={ProfileScreen} />
           </Tab.Navigator>
