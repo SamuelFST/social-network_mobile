0c787902359cc5dbe6e6132f4f06ab11b85dfc60
SamuelFST <guti.samuel2002@gmail.com>
2022-08-10 23:11:26 -0300

    feat: customAvatar, CustomIconButton and FavoriteIconButton components

---
+++ components/FavoriteIconButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import CustomIconButton from "./CustomIconButton";
+
+interface Props {
+  handleLike: () => void;
+  liked: boolean;
+}
+
+export default function FavoriteIconButton({ liked, handleLike }: Props) {
+  return (
+    <CustomIconButton handleOnPress={handleLike}>
+      {liked ? (
+        <MaterialIcons name="favorite" size={ 24 } color="red" />
+      ) : (
+        <MaterialIcons name="favorite-border" size={ 24 } />
+      )}
+    </CustomIconButton>
+  );
+}
