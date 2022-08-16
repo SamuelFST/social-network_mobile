f3073043e0414923c4483435304fa55386a68c11
SamuelFST <guti.samuel2002@gmail.com>
2022-08-16 12:09:12 -0300

    chore: removed the TouchableOpacity, added likePost and unlikePost actions

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Card } from "@rneui/base";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -6,33 +6,38 @@ import { MaterialIcons } from "@expo/vector-icons";
 import CustomAvatar from "./CustomAvatar";
 import FavoriteIconButton from "./FavoriteIconButton";
 import { Post } from "../models/Post";
+import { Context as PostContext } from "../context/PostContext";
 
 interface Props {
   post: Post;
 }
 
 export default function PostItem({ post }: Props) {
+  const { likePost, unlikePost } = useContext(PostContext);
+
   return (
-    <TouchableOpacity>
-      <Card>
-        <View style={styles.cardHeader}>
-          <CustomAvatar name={post.profile.name} />
-          <Text style={styles.cardTitle}>{post.title}</Text>
-        </View>
-        {post.image ? (
-          <Card.Image source={{ uri: post.description }} style={styles.cardImage} />
-        ) : (
-          <Text style={styles.cardDescription}>{post.description}</Text>
-        )}
-        <Card.Divider />
-        <View style={styles.actionContainer}>
-          <FavoriteIconButton liked={false} handleLike={() => { }} />
-          <Text style={styles.actionItem}>{post.likes.length}</Text>
-          <MaterialIcons name="chat-bubble-outline" size={24} style={styles.actionItem} />
-          <Text style={styles.actionItem}>{post.comments.length}</Text>
-        </View>
-      </Card>
-    </TouchableOpacity>
+    <Card>
+      <View style={styles.cardHeader}>
+        <CustomAvatar name={post.profile.name} />
+        <Text style={styles.cardTitle}>{post.title}</Text>
+      </View>
+      {post.image ? (
+        <Card.Image source={{ uri: post.description }} style={styles.cardImage} />
+      ) : (
+        <Text style={styles.cardDescription}>{post.description}</Text>
+      )}
+      <Card.Divider />
+      <View style={styles.actionContainer}>
+        <FavoriteIconButton liked={post.liked} handleLike={() => {
+          post.liked ?
+            unlikePost && unlikePost({ id: post._id })
+            : likePost && likePost({ id: post._id });
+        }} />
+        <Text style={styles.actionItem}>{post.likes.length}</Text>
+        <MaterialIcons name="chat-bubble-outline" size={24} style={styles.actionItem} />
+        <Text style={styles.actionItem}>{post.comments.length}</Text>
+      </View>
+    </Card>
   );
 }
 
