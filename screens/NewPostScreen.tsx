345ef63d501727a2fc2540e126992a4b53babc5d
SamuelFST <guti.samuel2002@gmail.com>
2022-08-16 12:13:12 -0300

    feat: newPost and Post screens

--- screens/NewPostScreen.tsx
+++ screens/NewPostScreen.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Input, Button } from "@rneui/base";
+
+import Spacer from "../components/Spacer";
+import PostImagePicker from "../components/PostImagePicker";
+import { File } from "../models/File";
+import { Context as PostContext } from "../context/PostContext";
 
 export default function NewPostScreen() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState<File>()
+  const { createPost } = useContext(PostContext);
+
   return (
     <View>
-      <Text>NewPostScreen</Text>
+      <Spacer>
+        <>
+          <Input
+            label="Título"
+            value={title}
+            onChangeText={setTitle}
+            autoCorrect={false}
+          />
+          <Spacer />
+          {image ? null : (
+            <Input
+              label="O que está acontecendo?"
+              value={description}
+              onChangeText={setDescription}
+              numberOfLines={3}
+              autoCorrect={false}
+            />
+          )}
+          <PostImagePicker onFileLoaded={setImage} />
+          <Spacer />
+          <Button
+            title="Publicar"
+            onPress={() => {createPost && createPost({ title, description, image })}}
+          />
+        </>
+      </Spacer>
     </View>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
 
 });
--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,14 +1,123 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, FlatList, KeyboardAvoidingView, ScrollView, SafeAreaView } from "react-native";
+import { Input, Button, Text, Divider } from "@rneui/base";
+import * as SecureStore from "expo-secure-store";
 
-export default function PostScreen() {
+import axios from "../api/axios";
+import PostItem from "../components/PostItem";
+import { Post } from "../models/Post";
+import Spacer from "../components/Spacer";
+import CustomAvatar from "../components/CustomAvatar";
+
+interface Props {
+  route: any;
+}
+
+export default function PostScreen({ route }: Props) {
+  const { id } = route.params;
+  const [post, setPost] = useState<Post>();
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("token");
+        const profile = await SecureStore.getItemAsync("profile");
+
+        const response = await axios.get(`/posts/${id}`, {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        });
+
+        const post = {
+          ...response.data,
+          liked: response.data.likes.includes(profile),
+        };
+
+        setPost(post);
+      } catch (err) {
+
+      }
+    }
+
+    getPost();
+  }, []);
+
+  const addComment = async () => {
+    try {
+      const token = await SecureStore.getItemAsync("token");
+
+      await axios.post(
+        `/posts/${id}/comments`,
+        { description },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (err) {
+
+    }
+  };
+
   return (
-    <View>
-      <Text>PostScreen</Text>
-    </View>
+    <>
+      {post ? (
+        <KeyboardAvoidingView>
+            <PostItem post={post} />
+            <Spacer>
+              <>
+                <Divider />
+                <Text h4 style={{ marginTop: 6 }}>Comentários</Text>
+                <Input
+                  label="Novo comentário"
+                  value={description}
+                  onChangeText={setDescription}
+                  autoCorrect={false}
+                />
+                <Button
+                  title="Publicar"
+                  onPress={() => {
+                    setDescription("");
+                    addComment();
+                  }}
+                />
+                <FlatList
+                nestedScrollEnabled
+                  style={styles.commentsContainer}
+                  data={post.comments}
+                  keyExtractor={({ _id }) => _id}
+                  renderItem={({ item }) => {
+                    return (
+                      <View style={styles.comment}>
+                        <CustomAvatar name={item.profile.name} />
+                        <Text style={styles.commentText}>{item.description}</Text>
+                      </View>
+                    );
+                  }}
+                />
+              </>
+            </Spacer>
+        </KeyboardAvoidingView>
+      ) : (null)}
+    </>
   );
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  commentsContainer: {
+    marginTop: 20,
+  },
+  comment: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  commentText: {
+    fontSize: 15,
+    marginLeft: 10,
+  },
 });
